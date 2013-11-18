066dfdaf5adf73ff8d18a3ff91ad566a4f89c5f9
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-18 18:58:41 +0000

    Added actionbarsherlock and actionscontentview styles and images

--- Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
@@ -1,10 +1,16 @@
 package com.donskifarrell.Hubblog.Activities;
 
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
+import com.google.inject.Inject;
+
 /**
  * Created with IntelliJ IDEA.
  * User: donski
  * Date: 18/11/13
  * Time: 17:48
  */
-public class BaseActivity {
+public class BaseActivity extends RoboSherlockFragmentActivity {
+    @Inject
+    public com.donskifarrell.Hubblog.Data.Hubblog hubblog;
+
 }
