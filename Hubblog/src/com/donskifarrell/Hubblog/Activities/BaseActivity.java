159a0f6560d8dd5e1fee03e7d1c9efe797d55aad
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-18 21:18:49 +0000

    Can dynamically add list of sites and their articles to sidebar.

--- Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
@@ -1,7 +1,21 @@
 package com.donskifarrell.Hubblog.Activities;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.donskifarrell.Hubblog.Adapters.ArticlesAdapter;
+import com.donskifarrell.Hubblog.Data.Account;
+import com.donskifarrell.Hubblog.Data.Post;
+import com.donskifarrell.Hubblog.Data.Site;
+import com.donskifarrell.Hubblog.R;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
 import com.google.inject.Inject;
+
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,4 +27,98 @@ public class BaseActivity extends RoboSherlockFragmentActivity {
     @Inject
     public com.donskifarrell.Hubblog.Data.Hubblog hubblog;
 
+    private static final String STATE_POSITION = "state:layout_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // todo: remove
+        bootstrap();
+
+        setContentView(R.layout.base_layout);
+
+        addSitesToSidebar();
+
+        final int selectedPosition;
+        if (savedInstanceState != null) {
+            selectedPosition = savedInstanceState.getInt(STATE_POSITION, 0);
+        } else {
+            selectedPosition = 0;
+        }
+
+        showArticle(selectedPosition);
+    }
+
+    private void addSitesToSidebar(){
+        LayoutInflater sidebar_list_inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.sidebar_layout);
+
+        for (Site site : hubblog.getSites()){
+            View sidebar_list = sidebar_list_inflater.inflate(R.layout.sidebar_list_layout, null);
+
+            TextView header = (TextView) sidebar_list.findViewById(R.id.header_title);
+            header.setText(site.getSiteName());
+
+            ListView articlesList = (ListView) sidebar_list.findViewById(R.id.articles);
+            articlesList.setAdapter(new ArticlesAdapter(this, site));
+            articlesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapter, View v, int position, long flags) {
+                    showArticle(position);
+                }
+            });
+
+            relativeLayout.addView(sidebar_list);
+        }
+    }
+
+    private void showArticle(int position){
+
+    }
+
+    private void bootstrap() {
+        Account acc = new Account();
+        acc.setAccountName("TestAccount1");
+        acc.setUsername("TestUsername");
+        acc.setPassword("TestPassword");
+        hubblog.addAccount(acc);
+
+        Site site = new Site();
+        site.setAccountName(acc.getAccountName());
+        site.setSiteName("TestSiteName");
+        hubblog.addSite(site);
+
+        Post post = new Post();
+        post.setSite(site.getSiteName());
+        post.setTitle("TestPost1 - Site 1");
+        post.setCreatedDate(new Date());
+        post.setContent("# Heading1 - post 1");
+        hubblog.addPostToSite(site, post);
+
+        post = new Post();
+        post.setSite(site.getSiteName());
+        post.setTitle("TestPost2 - Site 1");
+        post.setCreatedDate(new Date());
+        post.setContent("## Heading2 - post 2");
+        hubblog.addPostToSite(site, post);
+
+        site.setAccountName(acc.getAccountName());
+        site.setSiteName("TestSiteName 2");
+        hubblog.addSite(site);
+
+        post = new Post();
+        post.setSite(site.getSiteName());
+        post.setTitle("TestPost3 - Site 2");
+        post.setCreatedDate(new Date());
+        post.setContent("### Heading3 - post 3");
+        hubblog.addPostToSite(site, post);
+
+        post = new Post();
+        post.setSite(site.getSiteName());
+        post.setTitle("TestPost2 - Site 2");
+        post.setCreatedDate(new Date());
+        post.setContent("#### Heading4 - post 4");
+        hubblog.addPostToSite(site, post);
+    }
 }
---
+++ Hubblog/src/com/donskifarrell/Hubblog/Adapters/ArticlesAdapter.java
@@ -0,0 +1,68 @@
+package com.donskifarrell.Hubblog.Adapters;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.donskifarrell.Hubblog.Data.Site;
+import com.donskifarrell.Hubblog.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: donski
+ * Date: 18/11/13
+ * Time: 20:26
+ */
+public class ArticlesAdapter extends BaseAdapter {
+    private final Site site;
+    private final LayoutInflater inflater;
+
+    public ArticlesAdapter(Context context, Site aSite) {
+        inflater = LayoutInflater.from(context);
+        site = aSite;
+    }
+
+    @Override
+    public int getCount() {
+        return site.getPostsTitleList().length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return site.getPosts().get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.sidebar_list_item, parent, false);
+
+            holder = new ViewHolder();
+            holder.text = (TextView) convertView.findViewById(R.id.sidebar_item);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        final Drawable icon = convertView.getContext().getResources().getDrawable(R.drawable.ic_link_evernote);
+        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
+        holder.text.setCompoundDrawables(icon, null, null, null);
+        holder.text.setText(site.getPostsTitleList()[position]);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView text;
+    }
+}
