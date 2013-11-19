84e2c34917137ed300450f0e963eca0f1df6d638
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-19 15:36:06 +0000

    Added sidebar item highlight on click

--- Hubblog/src/com/donskifarrell/Hubblog/Adapters/ArticlesAdapter.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Adapters/ArticlesAdapter.java
@@ -5,8 +5,10 @@ import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.donskifarrell.Hubblog.Data.Post;
 import com.donskifarrell.Hubblog.Data.Site;
 import com.donskifarrell.Hubblog.R;
@@ -20,6 +22,8 @@ import com.donskifarrell.Hubblog.R;
 public class ArticlesAdapter extends BaseAdapter {
     private final Site site;
     private final LayoutInflater inflater;
+    private static Drawable editIcon;
+    private static Drawable tickIcon;
 
     public ArticlesAdapter(Context context, Site aSite) {
         inflater = LayoutInflater.from(context);
@@ -55,23 +59,48 @@ public class ArticlesAdapter extends BaseAdapter {
             final Drawable icon;
 
             if (post.getIsDraft()) {
-                icon = convertView.getContext().getResources().getDrawable(R.drawable.edit_glow);
-                icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-                holder.articleTitle.setCompoundDrawables(icon, null, null, null);
+                holder.articleTitle.setCompoundDrawables(getEditIcon(convertView.getContext()), null, null, null);
             }
             else {
-                icon = convertView.getContext().getResources().getDrawable(R.drawable.checkmark);
-                icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-                holder.articleTitle.setCompoundDrawables(icon, null, null, null);
+                holder.articleTitle.setCompoundDrawables(getTickIcon(convertView.getContext()), null, null, null);
             }
 
             holder.articleTitle.setText(post.getTitle());
+            holder.articleTitle.setOnClickListener(new AdapterView.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(
+                            view.getContext(),
+                            ((TextView)view).getText(),
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
         return convertView;
+    }
+
+    private static Drawable getEditIcon(Context context){
+        if (editIcon == null) {
+            editIcon = context.getResources().getDrawable(R.drawable.edit_glow);
+            editIcon.setBounds(0, 0, editIcon.getIntrinsicWidth(), editIcon.getIntrinsicHeight());
+        }
+
+        return editIcon;
+    }
+
+    private static Drawable getTickIcon(Context context){
+        if (tickIcon == null) {
+            tickIcon = context.getResources().getDrawable(R.drawable.checkmark);
+            tickIcon.setBounds(0, 0, tickIcon.getIntrinsicWidth(), tickIcon.getIntrinsicHeight());
+        }
+
+        return tickIcon;
     }
 
     private class ViewHolder {
