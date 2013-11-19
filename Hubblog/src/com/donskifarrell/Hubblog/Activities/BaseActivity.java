cc29761b77ef78cf6d1aac71b4bd5e8cca376417
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-19 10:17:12 +0000

    Updated bootstrapping method

--- Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Activities/BaseActivity.java
@@ -1,11 +1,7 @@
 package com.donskifarrell.Hubblog.Activities;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.*;
-import com.donskifarrell.Hubblog.Adapters.ArticlesAdapter;
 import com.donskifarrell.Hubblog.Adapters.SidebarAdapter;
 import com.donskifarrell.Hubblog.Data.Account;
 import com.donskifarrell.Hubblog.Data.Post;
@@ -14,6 +10,7 @@ import com.donskifarrell.Hubblog.R;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
 import com.google.inject.Inject;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 
 /**
@@ -55,48 +52,143 @@ public class BaseActivity extends RoboSherlockFragmentActivity {
 
     }
 
+    EnglishNumberToWords numberToWords;
+
     private void bootstrap() {
+        numberToWords = new EnglishNumberToWords();
+
         Account acc = new Account();
         acc.setAccountName("TestAccount1");
         acc.setUsername("TestUsername");
         acc.setPassword("TestPassword");
         hubblog.addAccount(acc);
 
-        Site site = new Site();
-        site.setAccountName(acc.getAccountName());
-        site.setSiteName("THE FIRST SITE");
-        hubblog.addSite(site);
+        for (int siteCount = 0; siteCount < 4; siteCount++){
+            Site site = createSite(acc, siteCount);
+            hubblog.addSite(site);
 
+            for (int postCount = 0; postCount < 8; postCount++){
+                Post post = createPost(site, postCount);
+                hubblog.addPostToSite(site, post);
+            }
+        }
+    }
+
+    private Site createSite(Account account, int idx){
+        Site site = new Site();
+        site.setAccountName(account.getAccountName());
+        site.setSiteName("THE SITE " + numberToWords.convertLessThanOneThousand(idx));
+
+        return site;
+    }
+
+    private Post createPost(Site site, int idx){
         Post post = new Post();
         post.setSite(site.getSiteName());
-        post.setTitle("TestPost1 - Site 1");
+        post.setTitle("A post " + numberToWords.convertLessThanOneThousand(idx));
         post.setCreatedDate(new Date());
-        post.setContent("# Heading1 - post 1");
-        hubblog.addPostToSite(site, post);
+        post.setContent("## Heading2 for post " + numberToWords.convertLessThanOneThousand(idx));
 
-        post = new Post();
-        post.setSite(site.getSiteName());
-        post.setTitle("TestPost2 - Site 1");
-        post.setCreatedDate(new Date());
-        post.setContent("## Heading2 - post 2");
-        hubblog.addPostToSite(site, post);
+        return post;
+    }
 
-        site.setAccountName(acc.getAccountName());
-        site.setSiteName("SECOND SITE");
-        hubblog.addSite(site);
+    private class EnglishNumberToWords {
 
-        post = new Post();
-        post.setSite(site.getSiteName());
-        post.setTitle("TestPost3 - Site 2");
-        post.setCreatedDate(new Date());
-        post.setContent("### Heading3 - post 3");
-        hubblog.addPostToSite(site, post);
+        private final String[] tensNames = { "", " ten", " twenty",
+                " thirty", " forty", " fifty", " sixty", " seventy", " eighty",
+                " ninety" };
 
-        post = new Post();
-        post.setSite(site.getSiteName());
-        post.setTitle("TestPost2 - Site 2");
-        post.setCreatedDate(new Date());
-        post.setContent("#### Heading4 - post 4");
-        hubblog.addPostToSite(site, post);
+        private final String[] numNames = { "", " one", " two", " three",
+                " four", " five", " six", " seven", " eight", " nine", " ten",
+                " eleven", " twelve", " thirteen", " fourteen", " fifteen",
+                " sixteen", " seventeen", " eighteen", " nineteen" };
+
+        private String convertLessThanOneThousand(int number) {
+            String soFar;
+
+            if (number % 100 < 20) {
+                soFar = numNames[number % 100];
+                number /= 100;
+            } else {
+                soFar = numNames[number % 10];
+                number /= 10;
+
+                soFar = tensNames[number % 10] + soFar;
+                number /= 10;
+            }
+            if (number == 0)
+                return soFar;
+            return numNames[number] + " hundred" + soFar;
+        }
+
+        public String convert(long number) {
+            // 0 to 999 999 999 999
+            if (number == 0) {
+                return "zero";
+            }
+
+            String snumber = Long.toString(number);
+
+            // pad with "0"
+            String mask = "000000000000";
+            DecimalFormat df = new DecimalFormat(mask);
+            snumber = df.format(number);
+
+            // XXXnnnnnnnnn
+            int billions = Integer.parseInt(snumber.substring(0, 3));
+            // nnnXXXnnnnnn
+            int millions = Integer.parseInt(snumber.substring(3, 6));
+            // nnnnnnXXXnnn
+            int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
+            // nnnnnnnnnXXX
+            int thousands = Integer.parseInt(snumber.substring(9, 12));
+
+            String tradBillions;
+            switch (billions) {
+                case 0:
+                    tradBillions = "";
+                    break;
+                case 1:
+                    tradBillions = convertLessThanOneThousand(billions) + " billion ";
+                    break;
+                default:
+                    tradBillions = convertLessThanOneThousand(billions) + " billion ";
+            }
+            String result = tradBillions;
+
+            String tradMillions;
+            switch (millions) {
+                case 0:
+                    tradMillions = "";
+                    break;
+                case 1:
+                    tradMillions = convertLessThanOneThousand(millions) + " million ";
+                    break;
+                default:
+                    tradMillions = convertLessThanOneThousand(millions) + " million ";
+            }
+            result = result + tradMillions;
+
+            String tradHundredThousands;
+            switch (hundredThousands) {
+                case 0:
+                    tradHundredThousands = "";
+                    break;
+                case 1:
+                    tradHundredThousands = "one thousand ";
+                    break;
+                default:
+                    tradHundredThousands = convertLessThanOneThousand(hundredThousands)
+                            + " thousand ";
+            }
+            result = result + tradHundredThousands;
+
+            String tradThousand;
+            tradThousand = convertLessThanOneThousand(thousands);
+            result = result + tradThousand;
+
+            // remove extra spaces!
+            return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
+        }
     }
 }
