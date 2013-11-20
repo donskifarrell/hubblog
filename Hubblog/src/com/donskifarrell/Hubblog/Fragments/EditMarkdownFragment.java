567f66bb6f1546076b5ba0514825e50cc506eb71
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-20 00:48:19 +0000

    Changed edit markdown to use scrollview to provide better scrolling experience

--- Hubblog/src/com/donskifarrell/Hubblog/Fragments/EditMarkdownFragment.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Fragments/EditMarkdownFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 import com.donskifarrell.Hubblog.R;
 import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockFragment;
 
@@ -17,7 +18,11 @@ public class EditMarkdownFragment extends RoboSherlockFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.edit_markdown_layout, container, false);
+        ScrollView scrollView = (ScrollView) inflater.inflate(R.layout.edit_markdown_layout, container, false);
+        scrollView.setSmoothScrollingEnabled(true);
+
+
+        return scrollView;
     }
 
 }
