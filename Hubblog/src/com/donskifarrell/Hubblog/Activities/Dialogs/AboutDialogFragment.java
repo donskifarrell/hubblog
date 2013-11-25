0907b298bc637da60b53ad821659e3868303aa2f
donskifarrell <donskifarrell+git@googlemail.com>
2013-11-25 22:20:49 +0000

    Updated about dialog with scroll view

--- Hubblog/src/com/donskifarrell/Hubblog/Activities/Dialogs/AboutDialogFragment.java
+++ Hubblog/src/com/donskifarrell/Hubblog/Activities/Dialogs/AboutDialogFragment.java
@@ -9,7 +9,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import com.donskifarrell.Hubblog.R;
 
 /**
@@ -19,14 +19,15 @@ import com.donskifarrell.Hubblog.R;
  * Time: 20:55
  */
 public class AboutDialogFragment extends DialogFragment {
-    private static final String url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WX48B6TLH773Q&lc=GB&item_name=Hubblog&item_number=hubblog&currency_code=GBP&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
+    private static final String url =
+            "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WX48B6TLH773Q&lc=GB&item_name=Hubblog&item_number=hubblog&currency_code=GBP&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        LinearLayout about = (LinearLayout) inflater.inflate(R.layout.dialog_about_layout, null);
+        ScrollView about = (ScrollView) inflater.inflate(R.layout.dialog_about_layout, null);
 
         ImageButton donate = (ImageButton) about.findViewById(R.id.donate_button);
         donate.setOnClickListener(new View.OnClickListener() {
